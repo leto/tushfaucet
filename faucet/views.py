84099693f604bf32a4dd326d73e9227f0e9450f8
Jay Graber <jay@z.cash>
2017-04-20 10:16:22 -0700

    Add changes to views to support shielded txs

--- faucet/views.py
+++ faucet/views.py
@@ -3,11 +3,26 @@ from django.http import HttpResponse
 from django.db.models import Q
 from django.utils import timezone
 
+import re
 from datetime import *
 
 from pyZcash.rpc.ZDaemon import *
 from faucet.models import *
 
+# TODO: Going to show the page no matter what, so pull these variables out.
+if HealthCheck.objects.latest('timestamp'):
+    hc = HealthCheck.objects.latest('timestamp')
+    balance = hc.balance
+    difficulty = hc.difficulty
+    height = hc.height
+    payouts = Drip.objects.count()
+else:
+    balance = '0'
+    difficulty = '0'
+    height = '0'
+    #TODO: where to put this?
+zd = ZDaemon()
+version = zd.getVersion()
 
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
@@ -18,59 +33,65 @@ def get_client_ip(request):
     return ip
 
 def index(request):
-	# TODO: Going to show the page no matter what, so pull these variables out.
-	if HealthCheck.objects.latest('timestamp'):
-		hc = HealthCheck.objects.latest('timestamp')
-		balance = hc.balance
-		difficulty = hc.difficulty
-		height = hc.height
-		payouts = Drip.objects.count()
-	else:
-		balance = '0'
-		difficulty = '0'
-		height = '0'
-	#TODO: where to put this?
-	zd = ZDaemon()
-	version = zd.getVersion()
-
-	#If it is a post, an address was submitted.
-	if request.method == 'POST':
-		# Check IP and payout address
-		# This is the old workaround, added get_client_ip
-		# ip = request.META.get('REMOTE_ADDR')
+    #If it is a post, an address was submitted.
+    if request.method == 'POST':
+        # Check IP and payout address
+        # This is the old workaround, added get_client_ip
+        # ip = request.META.get('REMOTE_ADDR')
         # if ip == '127.0.0.1':
         #     ip = request.META.get('HTTP_X_REAL_IP')
-		ip = get_client_ip(request)
-		address = request.POST.get('address', '')
-		print "IP: ", ip
-		print "address: ", address
-		try:
-			last_payout = Drip.objects.filter(Q(ip=ip) | Q(address=address)).order_by('-timestamp')[0]
-        		now = datetime.utcnow().replace(tzinfo=timezone.get_current_timezone())
-			timesince = (now - last_payout.timestamp).total_seconds()
+        ip = get_client_ip(request)
+        address = request.POST.get('address', '')
+        print "IP: ", ip
+        print "address: ", address
+        try:
+            last_payout = Drip.objects.filter(Q(ip=ip) | Q(address=address)).order_by('-timestamp')[0]
+            now = datetime.utcnow().replace(tzinfo=timezone.get_current_timezone())
+            timesince = (now - last_payout.timestamp).total_seconds()
 
-			# TODO: keep track of sessions as well, track one per session?
+            # TODO: keep track of sessions as well, track one per session?
 
-			if timesince < (60*60*12):
-				return render(request, 'faucet/faucet.html', {'version':version,'balance':balance,'difficulty':difficulty,'height':height, 'payouts':payouts, 'flash':True, 'message':"Sorry, you received a payout too recently.  Come back later."})
+            if timesince < (60*60*12):
+                msg = "Sorry, you received a payout too recently.  Come back later."
+                return render(request, 'faucet/faucet.html', {'version':version,'balance':balance,'difficulty':difficulty,'height':height, 'payouts':payouts, 'flash':True, 'message':msg})
 
-		except (Drip.DoesNotExist, IndexError) as e:
-			# Nothing in queryset, so we've never seen this ip and address before (individually)
-			pass
+        except (Drip.DoesNotExist, IndexError) as e:
+            # Nothing in queryset, so we've never seen this ip and address before (individually)
+            pass
 
-		# zd = ZDaemon()
-		tx = zd.sendTransparent(address, 0.1)
+        # zd = ZDaemon()
+        try:
+            # Did the tx work?
+            if len(address) == len('tmKBPqa8qqKA7vrGq1AaXHSAr9vqa3GczzK'):
+                tx = zd.sendtoaddress(address, 0.1)
+                if len(tx) == len('2ac64e297e3910e7ffda7210e7aa2463fe2ec5f69dfe7fdf0b4b9be138a9bfb8'):
+                    #Save Drip.
+                    drip = Drip(address=address,txid=tx,ip=ip)
+                    drip.save()
+                    msg = "Sent! txid: {0}</br>View your transaction on the testnet explorer.".format(tx)
+                    return render(request, 'faucet/faucet.html', {'version':version,'balance':balance,'difficulty':difficulty,'height':height, 'payouts':payouts, 'flash':True, 'message':msg})
+            elif len(address) == len('ztbx5DLDxa5ZLFTchHhoPNkKs57QzSyib6UqXpEdy76T1aUdFxJt1w9318Z8DJ73XzbnWHKEZP9Yjg712N5kMmP4QzS9iC9'):
+                # sender = 'ztbx5DLDxa5ZLFTchHhoPNkKs57QzSyib6UqXpEdy76T1aUdFxJt1w9318Z8DJ73XzbnWHKEZP9Yjg712N5kMmP4QzS9iC9'
+                sender = 'tmQWmuaD3kAZ5U1NN1nFJJh5h7QQeMQ46xC'
+                msg = 'Thanks for using zfaucet!'.encode('hex')
+                opid = zd.z_sendmany(sender, address, 0.01, msg)
+                print "OPID", opid
+                if opid != None and 'opid' in opid:
+                        resp = zd.z_getoperationstatus(opid)
+                        print "Operation status response:", resp
+                        print "operation status: ", resp[0]['status']
+                        #why is it not working when it's executing?
+                        if resp[0]['status'] == 'executing':
+                            msg = "Sent! Get the status of your shielded funds with z_getoperationstatus {0}.".format(opid)
+                            return render(request, 'faucet/faucet.html', {'version':version,'balance':balance,'difficulty':difficulty,'height':height, 'payouts':payouts, 'flash':True, 'message':msg})
+                        if resp[0]['status'] == 'failed':
+                            msg = "Operation failed for {0}. Error message: {1}".format(opid, resp[0]['error']['message'])
+                            return render(request, 'faucet/faucet.html', {'version':version,'balance':balance,'difficulty':difficulty,'height':height, 'payouts':payouts, 'flash':True, 'message':msg})
+        except:
+            # TODO: Give better error if faucet is empty!
+            print "IN ERROR"
+            msg = "Issue sending transaction.  Is your address correct?"
+            return render(request, 'faucet/faucet.html', {'version':version,'balance':balance,'difficulty':difficulty,'height':height, 'payouts':payouts, 'flash':True, 'message':msg})
 
-		# TODO: Give better error if faucet is empty!
 
-		#Did the tx work?
-		if tx:
-			#Save Drip.
-			drip = Drip(address=address,txid=tx,ip=ip)
-			drip.save()
-			return render(request, 'faucet/faucet.html', {'version':version,'balance':balance,'difficulty':difficulty,'height':height, 'payouts':payouts, 'flash':True, 'message':"Sent! txid: " + tx})
-		else:
-			return render(request, 'faucet/faucet.html', {'version':version,'balance':balance,'difficulty':difficulty,'height':height, 'payouts':payouts, 'flash':True, 'message':"Issue sending transaction.  Is your address correct?"})
-
-
-	return render(request, 'faucet/faucet.html', {'version':version,'balance':balance,'difficulty':difficulty,'height':height, 'payouts':payouts, 'flash':False, 'message':""})
+    return render(request, 'faucet/faucet.html', {'version':version,'balance':balance,'difficulty':difficulty,'height':height, 'payouts':payouts, 'flash':False, 'message':""})
