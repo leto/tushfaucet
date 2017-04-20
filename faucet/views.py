6a1becfbfd234758c064ae5ed2c653f2a04ea7a2
Jay Graber <jay@z.cash>
2017-04-20 10:30:48 -0700

    Send to zaddr from taddr with spendable utxo

--- faucet/views.py
+++ faucet/views.py
@@ -72,7 +72,7 @@ def index(request):
                     return render(request, 'faucet/faucet.html', {'version':version,'balance':balance,'difficulty':difficulty,'height':height, 'payouts':payouts, 'flash':True, 'message':msg})
             elif len(address) == len('ztbx5DLDxa5ZLFTchHhoPNkKs57QzSyib6UqXpEdy76T1aUdFxJt1w9318Z8DJ73XzbnWHKEZP9Yjg712N5kMmP4QzS9iC9'):
                 # sender = 'ztbx5DLDxa5ZLFTchHhoPNkKs57QzSyib6UqXpEdy76T1aUdFxJt1w9318Z8DJ73XzbnWHKEZP9Yjg712N5kMmP4QzS9iC9'
-                sender = 'tmQWmuaD3kAZ5U1NN1nFJJh5h7QQeMQ46xC'
+                sender = zd.findTaddrWithUnspent()
                 msg = 'Thanks for using zfaucet!'
                 opid = zd.z_sendmany(sender, address, 0.01, msg)
                 print "OPID", opid
