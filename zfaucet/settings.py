b6b79283e82f993a01bc5ffe2e613c5490599833
Jay Graber <jay@z.cash>
2017-04-26 12:59:39 -0700

    Change settings.py for prod

--- zfaucet/settings.py
+++ zfaucet/settings.py
@@ -15,10 +15,11 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+ENVIRONMENT = os.getenv('DJANGO_ENVIRONMENT', 'prod')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
-ENVIRONMENT = os.getenv('DJANGO_ENVIRONMENT', 'dev')
+# ENVIRONMENT = os.getenv('DJANGO_ENVIRONMENT', 'dev')
 if ENVIRONMENT not in ['dev', 'stage', 'prod']:
         raise Exception('Unknown settings environment "%s"' % ENVIRONMENT)
 print 'settings environment is ' + ENVIRONMENT
